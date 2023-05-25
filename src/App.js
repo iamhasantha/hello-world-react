efb3472b7f6af672fd03f5f24b5e1522ac0c8764
Hasantha Kariyawasam <2020cs090@stu.ucsc.cmb.ac.lk>
2023-05-25 15:42:06 +0530

    set State in React

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import MyComponent from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
     <div className="App">
-      <Message name="Hasantha"/>
+      <Counter/>
+      {/* <Message name="Hasantha"/> */}
       {/* <MyComponent name="Hasantha"/> */}
       {/* <Welcome/> */}
       {/* <Hello/> */}
